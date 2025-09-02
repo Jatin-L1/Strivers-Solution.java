f6315db38851de0e75ecbfc22bae146cd6e9212e
Jatin-L1 <jatinsharmasm2435@gmail.com>
2025-09-03 00:10:22 +0530

    Binary search started

---
+++ BinarySearch/BinarySearchOn1D/BinarySearch.java
@@ -0,0 +1,21 @@
+package BinarySearch.BinarySearchOn1D;
+
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+
+        int lo = 0  , hi = nums.length;
+
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) hi = mid-1;
+            else lo= mid+1;
+        }
+        return -1;
+
+        
+    }
+    
+}
