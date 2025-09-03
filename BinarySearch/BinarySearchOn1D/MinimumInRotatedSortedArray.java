8e64e3aa2bda826bab7cabda6e1c52ff3fc036e6
Jatin-L1 <jatinsharmasm2435@gmail.com>
2025-09-04 00:53:46 +0530

    More binary search questions

--- BinarySearch/BinarySearchOn1D/MinimumInRotatedSortedArray.java
+++ BinarySearch/BinarySearchOn1D/MinimumInRotatedSortedArray.java
@@ -1,6 +1,19 @@
 package BinarySearch.BinarySearchOn1D;
 
 public class MinimumInRotatedSortedArray {
+    public int findMin(int[] arr) {
+        int n = arr.length;
+        int lo =0 ; 
+        int hi = n-1;
+        int ans = Integer.MAX_VALUE;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid]>arr[hi]) lo = mid+1;
+            else hi = mid;
+        }
+        return arr[lo];
+        
+    }
     
     
 }
---
+++ BinarySearch/BinarySearchOn1D/TimesArrayIsRotated.java
@@ -0,0 +1,25 @@
+package BinarySearch.BinarySearchOn1D;
+
+public class TimesArrayIsRotated {
+    public int index(int[] arr){
+        int n = arr.length;
+        int lo=0;
+        int hi = n-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[lo]<=arr[mid]){
+                ans = Math.min(ans, arr[lo]);
+                lo = mid+1;
+            }
+            else{
+                ans = Math.min(ans, arr[mid]);
+                hi = mid - 1;
+            }
+
+        }
+        return ans;
+    }
+    
+}
